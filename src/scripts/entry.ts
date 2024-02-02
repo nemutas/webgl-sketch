8847fe166bdafd72fbc673866413b726a4f49e3e
nemutas <inohiro.focru@gmail.com>
2024-02-02 23:05:31 +0900

    init

--- src/scripts/entry.ts
+++ src/scripts/entry.ts
@@ -1,10 +1,5 @@
 import { Canvas } from './Canvas'
 
-const root = location.pathname
-  .split('/')
-  .filter((s) => s !== '')
-  .at(-1)
-
-import(import.meta.env.BASE_URL + `${root}/sketch.fs`).then((file) => {
-  new Canvas(document.querySelector<HTMLCanvasElement>('canvas')!, file.default)
-})
+export function createCanvas(fragmentShader: string) {
+  new Canvas(document.querySelector<HTMLCanvasElement>('canvas')!, fragmentShader)
+}
