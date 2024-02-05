f01879c4808249532572cbce57b0979d3c1fbbdb
nemutas <inohiro.focru@gmail.com>
2024-02-05 19:29:53 +0900

    fixed path

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,7 +1,8 @@
 export function setSourceLink() {
   const source = document.querySelector<HTMLAnchorElement>('.links .source')
   if (source) {
-    const page = location.href.split('/').at(-2)
+    const p = location.href.split('/')
+    const page = p[p.length - 2]
     source.href = `https://github.com/nemutas/webgl-sketch/blob/main/src/${page}/sketch.fs`
   }
 }
