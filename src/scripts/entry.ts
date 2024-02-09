d4d9b5fce5e6f55192d44c4b22d01daad57a9d86
nemutas <inohiro.focru@gmail.com>
2024-02-09 15:31:23 +0900

    コード整理

--- src/scripts/entry.ts
+++ src/scripts/entry.ts
@@ -1,5 +1,14 @@
 import { Canvas } from './Canvas'
 
-export function createCanvas(fragmentShader: string) {
+export function entry(fragmentShader: string) {
+  setSourceLink()
   new Canvas(document.querySelector<HTMLCanvasElement>('canvas')!, fragmentShader)
+}
+
+function setSourceLink() {
+  const source = document.querySelector<HTMLAnchorElement>('.links .source')
+  if (source) {
+    const page = location.href.split('/').at(-2)
+    source.href = `https://github.com/nemutas/webgl-sketch/blob/main/src/${page}/sketch.fs`
+  }
 }
--- src/scripts/utils.ts
+++
@@ -1,8 +0,0 @@
-export function setSourceLink() {
-  const source = document.querySelector<HTMLAnchorElement>('.links .source')
-  if (source) {
-    const p = location.href.split('/')
-    const page = p[p.length - 2]
-    source.href = `https://github.com/nemutas/webgl-sketch/blob/main/src/${page}/sketch.fs`
-  }
-}
