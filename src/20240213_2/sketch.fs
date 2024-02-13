391ae5585a940d6bc6fdc6355ae61928761a348c
nemutas <inohiro.focru@gmail.com>
2024-02-13 18:18:49 +0900

    20240213_2

--- src/20240213_2/sketch.fs
+++ src/20240213_2/sketch.fs
@@ -10,6 +10,7 @@ in vec2 vUv;
 out vec4 outColor;
 
 #define loop(n) for(int i; i < n; i++)
+#define sat(v) clamp(v, 0.0, 1.0)
 
 const float PI = acos(-1.0);
 
@@ -49,9 +50,6 @@ float sdCross(vec3 p) {
 }
 
 float sdf(vec3 p) {
-  p.y -= 0.05;
-  p.z -= lt * 0.5;
-
   p = mod(p, 2.0) - 1.0;
 
   float b = box(p, vec3(1.0));
@@ -68,28 +66,55 @@ float sdf(vec3 p) {
 }
 
 void main() {
-  vec2 uv = vUv, asp = resolution / min(resolution.x, resolution.y), suv = (uv * 2.0 - 1.0) * asp;
-  vec3 rd = normalize(vec3(suv, -5.0)), ro = vec3(0.0, 0.0, 7.0);
-
-  lt = time * 80.0 / 60.0;
+  lt = time * 120.0 / 60.0;
   bt = floor(lt);
   tt = tanh(fract(lt) * 4.0);
   lt = bt + tt;
+  float ft = fract(time);
 
   h = hash(vec3(bt));
+
+  vec2 uv = vUv, asp = resolution / min(resolution.x, resolution.y), suv = (uv * 2.0 - 1.0) * asp;
+
+  suv.x += sin(suv.y * 1000.0 + ft * 100.0 ) * 0.005;
+  vec2 fuv = floor(suv * vec2(1, 1.5) * 2.0);
+  vec3 fh = hash(vec3(fuv, time));
+  if (fh.x < 0.1) suv.x += sign(fh.y) * 0.05;
+
+  if (0.0 < mod(floor(suv.y * 0.8 - ft), 2.0)) {
+    suv.x += 0.05;
+  }
+
+  vec3 rd = normalize(vec3(suv, -5.0 + length(suv) * 2.0)), ro = vec3(0.0, 0.0, 7.0);
 
   float t, acc;
   loop(64) {
     vec3 p = rd * t + ro;
+    if (h.x < 0.9) {
+      p.y -= 0.1;
+      p.z -= lt * 0.6;
+    } else {
+      p.xy *= rot(t * 0.5);
+      p.z -= ft * 6.0;
+    }
     float d = sdf(p);
     if (d < 1e-4 || 1e4 < t) break;
     t += d;
     acc += exp(abs(d) * -300.0);
   }
 
-  float c = acc / 64.0 * 0.5;
+  float c;
+  if (h.y < 0.6) c = acc / 64.0 * 0.5;
+  else c = fwidth(acc) * 0.02;
 
-  outColor = vec4(vec3(c), 1.0);
+  float sc = step(fract(suv.y * 40.0 - fract(time) * 10.0), 0.8);
+  c += sc * 0.07;
+
+  vec4 b = texture(backBuffer, vUv);
+  vec3 col = vec3(sat(c), b.rg);
+  col = mix(col, b.rgb, 0.5);
+
+  outColor = vec4(col, 1.0);
 }
 
 //----------------------------
